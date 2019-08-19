ac22d8cfb1c6b17ab07cf0152d3bf3abb0c3cd3e
CarrotsWorking <carrotkutay@gmail.com>
2019-08-19 17:19:32 +0200

    adding three.js animating a cube

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -1,12 +1,33 @@
-console.log('hello world');
+import * as THREE from 'three';
 
-function component() {
-    const element = document.createElement('div');
+//setup camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//creating Geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    return element;
+//creating Material
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+
+//setup renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//adding canvas to the scene
+document.body.appendChild(renderer.domElement);
+
+//setup scene
+const scene = new THREE.Scene();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
-
-document.body.appendChild(component());
+animate();
