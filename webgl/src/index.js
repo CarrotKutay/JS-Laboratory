17144901afae4a4dfd118dba721f98cd08fa911d
CarrotsWorking <carrotkutay@gmail.com>
2019-08-21 10:57:55 +0200

    orbit controls working, but with wrong focus

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -1,33 +1,46 @@
-import * as THREE from 'three';
-
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls';
 //setup camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 //creating Geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new BoxGeometry(1, 1, 1);
 
 //creating Material
-const material = new THREE.MeshBasicMaterial({
+const material = new MeshBasicMaterial({
     color: 0x00ff00
 });
 
 //setup renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //adding canvas to the scene
 document.body.appendChild(renderer.domElement);
 
 //setup scene
-const scene = new THREE.Scene();
-const cube = new THREE.Mesh(geometry, material);
+const scene = new Scene();
+const cube = new Mesh(geometry, material);
+let controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 20, 100);
+controls.update();
+
 scene.add(cube);
+
 
 function animate() {
     requestAnimationFrame(animate);
+
+    //rotation
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    //rotating around cube
+    controls.update();
+
     renderer.render(scene, camera);
 }
 animate();
