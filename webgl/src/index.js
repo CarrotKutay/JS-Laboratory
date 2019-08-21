03d0bfa133cbc5db3581e4a876b192969dcad222
CarrotsWorking <carrotkutay@gmail.com>
2019-08-21 12:45:39 +0200

    addded antialiasing, OrbitControls working properly now with correct pivit

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -1,33 +1,45 @@
+const THREE = require('three');
 import {
     OrbitControls
 } from 'three/examples/jsm/controls/OrbitControls';
 //setup camera
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 //creating Geometry
-const geometry = new BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 //creating Material
-const material = new MeshBasicMaterial({
+const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00
 });
 
 //setup renderer
-const renderer = new WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: 'true'
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 
 //adding canvas to the scene
 document.body.appendChild(renderer.domElement);
 
 //setup scene
-const scene = new Scene();
-const cube = new Mesh(geometry, material);
-let controls = new OrbitControls(camera, renderer.domElement);
+const scene = new THREE.Scene();
+const cube = new THREE.Mesh(geometry, material);
 
-camera.position.set(0, 20, 100);
+//setup Orbit Controls
+let controls = new OrbitControls(camera, renderer.domElement);
+controls.target = cube.getWorldPosition(cube.position);
+controls.autoRotateSpeed = 5;
+controls.autoRotate = true;
 controls.update();
 
+//setup Lights
+const light = new THREE.PointLight(0xff0000, 1, 100);
+light.position.set(50, 50, 50);
+
+scene.add(light);
 scene.add(cube);
 
 
@@ -35,8 +47,8 @@ function animate() {
     requestAnimationFrame(animate);
 
     //rotation
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
 
     //rotating around cube
     controls.update();
